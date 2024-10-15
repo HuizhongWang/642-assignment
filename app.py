4df3e9e2573ead6283798699edc6ca07808d1196
Huizhong-Wang-1159528 <150647178+HuizhongWang@users.noreply.github.com>
2024-10-15 18:25:20 +1300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,flash,render_template,request,session,redirect,url_for,g
+from flask_hashing import Hashing
+
+app = Flask(__name__)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/blog")
+def lblog():
+    return render_template("blog.html")
+
+if __name__ == "__main__":
+    app.run()
